9885c72319899bfab7ca9d992ef97e7fe7827915
Akuru <anjalismith0529@gmail.com>
2022-05-25 19:18:53 -0400

    created index route for thoughts and users

---
+++ routes/api/index.js
@@ -0,0 +1,7 @@
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
+const router = require('express').Router();
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
+
+module.exports = router;
