9a04ceccdbe151b27680d27a18e57ad72e8d52e9
Akuru <anjalismith0529@gmail.com>
2022-05-25 19:46:02 -0400

    made edits

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,16 +1,16 @@
 const router = require('express').Router();
-const { getThoughts, getSingleThought, createThought, deleteThought, updateThought, addReaction, deleteReaction } = require('../../controllers/userController');
+const { getThoughts, getSingleThought, createThought, deleteThought, updateThought, addReaction, deleteReaction } = require('../../controllers/thoughtController');
 
 // All thoughts /api/thoughts
 // Find and create thoughts
 router.route('/').get(getThoughts).post(createThought);
 
-// One thought /api/thoughts/:thoughtid
+// One thought /api/:thoughtid
 // Update and delete a user
 router.route('/:thoughtId')
 .get(getSingleThought).put(updateThought).delete(deleteThought);
 
-// Thought reactions /api/thoughts/:thoughtid/reactions
+// Thought reactions /api/:thoughtid/reactions
 // Add and Delete a reaction
 router.route('/:thoughtId/reactions').post(addReaction).delete(deleteReaction);
 
