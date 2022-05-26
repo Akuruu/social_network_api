8a3becce394d7ecd3a2b8b322ba16281687ed2ba
Akuru <anjalismith0529@gmail.com>
2022-05-25 20:54:42 -0400

    fixed get method

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -6,31 +6,31 @@ const thoughtController = {
     
     // Get all thoughts
     getThoughts(req, res) {
-        Thought.find()
-          .then((thoughts) => res.json(thoughts))
-          .catch((err) => res.status(500).json(err));
-      },
+      Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+    },
 
     // Get a single thought
     getSingleThought(req, res) {
-
-        Thought.findOne({ _id: req.params.thoughtId })
-          .then((thoughts) =>
-            !thoughts
-            ? res.status(404).json({ message: 'Please enter a valid thought ID' })
-            : res.json(thoughts)
-          )
-          .catch((err) => res.status(500).json(err));
-      },
+      Thought.findOne({_id: req.params.thoughtId})
+      .select('-__v')
+      .then((thought) =>
+        !thought
+        ? res.status(404).json({ message: 'Please enter a valid thought ID' })
+        : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+    },
 
     // Create a thought
     createThought(req, res) {
 
       Thought.create(req.body)
-      .then((thoughts) => {
+      .then((thought) => {
           return User.findOneAndUpdate(
             {_id:req.body.userId},
-            {$addToSet:{thoughts:thoughts._id}},
+            {$addToSet:{thoughts:thought._id}},
             {new:true}
         );
       })
@@ -115,7 +115,7 @@ const thoughtController = {
         )
         .then((user) =>
             !user
-            ? res.status(404).json({ message: 'Please enter a valid thought' })
+            ? res.status(404).json({ message: 'Please enter a valid thought ID' })
             : res.json(user),
           )
         .catch((err) => {
